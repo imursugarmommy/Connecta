1586265310cdb4860259c178bab641af4ac08e9a
imursugarmommy <levi.besch@gmail.com>
2025-01-12 19:27:46 +0100

    correct dependencies and versions

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./app/(tabs)/**/*.{js,jsx,ts,tsx}",
-    "./app/users/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {

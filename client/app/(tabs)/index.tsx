fa42208c671bcb018fcf36215c1b6566c813c36f
imursugarmomy <levi.besch@gmail.com>
2025-01-13 14:14:13 +0100

    Post Template done

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,9 +1,54 @@
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import { useState } from "react";
+
+import PostTemplate from "../../components/PostTemplate";
 
 export default function HomeScreen() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "Eilmeldung",
+      content: "Younes ist schwul",
+      username: "Dragoneisbaer",
+      comments: 30,
+      likes: 5000,
+    },
+    {
+      id: 2,
+      title: "Post 2",
+      content: "Content 2",
+      username: "User 2",
+      comments: 2,
+      likes: 10,
+    },
+    {
+      id: 3,
+      title: "Post 3",
+      content: "Content 3",
+      username: "User 3",
+      comments: 0,
+      likes: 0,
+    },
+    {
+      id: 4,
+      title: "Post 4",
+      content: "Content 1",
+      username: "User 1",
+      comments: 0,
+      likes: 0,
+    },
+    {
+      id: 5,
+      title: "Post 5",
+      content: "Content 1",
+      username: "User 1",
+      comments: 0,
+      likes: 5,
+    },
+  ]);
+
   return (
     <View className="flex-1 items-center">
       <View
@@ -11,12 +56,19 @@ export default function HomeScreen() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <Text className="text-2xl font-bold">Neue Posts für dich</Text>
+      <View className="w-full p-4 items-center">
+        <Text className="text-2xl font-bold">Neue Posts für dich</Text>
+      </View>
       <View
-        className="my-8 h-px w-4/5"
+        className="h-px w-4/5"
         lightColor="#eee"
         darkColor="rgb(255, 255, 255)"
       />
+      <ScrollView className="w-full h-full p-4">
+        {posts.map((post) => (
+          <PostTemplate post={post} />
+        ))}
+      </ScrollView>
     </View>
   );
 }
---
+++ client/components/PostTemplate.tsx
@@ -0,0 +1,37 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+interface Post {
+  id: number;
+  username: string;
+  title: string;
+  content: string;
+  comments: number;
+  likes: number;
+}
+
+const Post = ({ post }: { post: Post }) => {
+  return (
+    <View
+      className="w-full bg-red-200 mb-6 rounded-md overflow-hidden"
+      key={post.id}>
+      <View className="w-full bg-gray-500 p-3">
+        <Text className="text-2xl text-white">{post.title}</Text>
+      </View>
+      <View className="w-full bg-white dark:bg-[#242732] h-32 items-center justify-center">
+        <Text className="text-xl">{post.content}</Text>
+      </View>
+      <View className="w-full flex-row bg-gray-500 p-3">
+        <View className="bg-transparent">
+          <Text className="text-white">{post.username}</Text>
+        </View>
+        <View className="flex-grow flex-row justify-end bg-transparent">
+          <Text className="mr-4 text-white">Kommentare: {post.comments}</Text>
+          <Text className="text-white">Likes: {post.likes}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Post;

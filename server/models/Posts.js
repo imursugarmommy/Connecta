6857ea76ef8ba22903d5b7153cd52a5990739df7
imursugarmommy <levi.besch@gmail.com>
2025-01-18 15:04:13 +0100

    add all neccessary sequelize models

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -1,26 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-    const Posts = sequelize.define('Posts', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        content: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        likes: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        comments: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
+  const Posts = sequelize.define("Posts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  Posts.associate = (models) => {
+    Posts.hasMany(models.Comments, {
+      onDelete: "cascade",
     });
 
-    return Posts;
-}
+    Posts.hasMany(models.Likes, {
+      onDelete: "cascade",
+    });
+
+    Posts.hasMany(models.Dislikes, {
+      onDelete: "cascade",
+    });
+  };
+
+  return Posts;
+};

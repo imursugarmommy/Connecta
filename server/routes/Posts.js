42426e768377a58de0179a81100d839ab1e21174
imursugarmommy <levi.besch@gmail.com>
2025-01-18 15:09:58 +0100

    reviewing Post route

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,16 +1,18 @@
-const express = require("express")
-const router = express.Router()
-const { Posts } = require("../models")
+const express = require("express");
+const router = express.Router();
+const { Posts } = require("../models");
 
 router.get("/", async (req, res) => {
-    const postsList = await Posts.findAll()
-    res.json(postsList)
+  const postList = await Posts.findAll();
+
+  res.json(postList);
 });
 
 router.post("/", async (req, res) => {
-    const post = req.body;
-    await Posts.create(post);
-    res.json(post);
+  const post = req.body;
+  await Posts.create(post);
+
+  res.json(post);
 });
 
-module.exports = router
+module.exports = router;

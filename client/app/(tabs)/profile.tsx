8c9e71d98cb9c03dff7094d4e2e39ad9ffd3030f
imursugarmommy <levi.besch@gmail.com>
2025-02-21 16:13:56 +0100

    integrate user posts fetching, and enhance UI

--- client/app/(tabs)/profile.tsx
+++ client/app/(tabs)/profile.tsx
@@ -1,34 +1,121 @@
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { useAuth } from "../helpers/AuthContext";
+import { router } from "expo-router";
 
-export default function ProfileScreen() {
+import { Pencil, Share, UserRound } from "lucide-react-native";
+import Divider from "../../components/ui/Divider";
+import axios from "axios";
+import PostTemplate from "@/components/PostTemplate";
+import { Post } from "@/types/Post";
+import { ScrollView } from "react-native-gesture-handler";
+
+function ProfilePage() {
+  const { authState } = useAuth();
+  const [userPosts, setUserPosts] = useState([]);
+
+  const serverip = process.env.EXPO_PUBLIC_SERVERIP;
+
+  if (!authState.state) router.push("/auth/login");
+
+  function editProfilePicture() {}
+
+  function shareProfile() {}
+
+  function editProfile() {}
+
+  useEffect(() => {
+    axios
+      .get(`http://${serverip}:6969/posts/byuserid/${authState.id}`)
+      .then((res) => {
+        setUserPosts(res.data);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <View style={{height: 10}} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.headline}>Profil</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgb(255, 255, 255)" />
-    </View>
+    <ScrollView className="flex-1 p-4 px-6 bg-white dark:bg-black">
+      <View className="flex gap-y-2 items-center">
+        <View className="flex-row w-full mb-2">
+          <TouchableOpacity
+            onPress={editProfilePicture}
+            className="justify-center mr-4">
+            <View className="h-20 w-20 items-center justify-end bg-gray-200 rounded-full">
+              <UserRound
+                size={55}
+                color={"white"}
+              />
+            </View>
+          </TouchableOpacity>
+
+          <View className="flex-grow flex-wrap">
+            <Text className="text-black dark:text-white text-3xl font-bold">
+              @{authState.username}
+            </Text>
+
+            <View className="flex-row flex-grow justify-between items-center">
+              <View className="flex items-center">
+                <Text className="text-lg">13</Text>
+                <Text className="font-bold">Posts</Text>
+              </View>
+              <View className="flex items-center">
+                <Text className="text-lg">96</Text>
+                <Text className="font-bold">Follower</Text>
+              </View>
+              <View className="flex items-center">
+                <Text className="text-lg">126</Text>
+                <Text className="font-bold">Following</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+
+        <View className="w-full flex-row justify-between gap-x-2">
+          <TouchableOpacity className="flex-row flex-grow justify-center items-center border border-[#ededed] dark:border-[#414450] rounded-lg py-1">
+            <Text className="text-lg text-black dark:text-white mr-2">
+              Edit Profile
+            </Text>
+            <Pencil
+              size={15}
+              color={"black"}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity className="flex-row flex-grow justify-center items-center border border-[#ededed] dark:border-[#414450] rounded-lg py-1">
+            <Text className="text-lg text-black dark:text-white mr-2">
+              Share Profile
+            </Text>
+            <Share
+              size={15}
+              color={"black"}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View className="my-4">
+        <Divider
+          orientation="horizontal"
+          text="Your Posts"
+        />
+      </View>
+
+      <View className="w-full">
+        {userPosts.map((post: Post) => {
+          return (
+            <View
+              key={post.id}
+              className="mb-4">
+              <PostTemplate
+                post={post}
+                handleSnapPress={() => {}}
+              />
+            </View>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  headline: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  }
-});
+export default ProfilePage;

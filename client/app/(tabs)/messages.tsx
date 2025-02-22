a0ea5e512148d575eef94be66303b92adfc0032e
imursugarmommy <levi.besch@gmail.com>
2025-02-22 19:04:26 +0100

    chat-overview-page design

--- client/app/(tabs)/messages.tsx
+++ client/app/(tabs)/messages.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
+import { router } from "expo-router";
 
-const messages = () => {
+const ChatList = () => {
+  const chats = [
+    { id: "1", name: "John Doe", lastMessage: "Hey, what's up?" },
+    { id: "2", name: "Jane Smith", lastMessage: "Let's catch up soon!" },
+  ];
+
   return (
-    <View>
-      <Text>messages</Text>
+    <View className="flex-1 bg-white">
+      <FlatList
+        data={chats}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            className="p-2 py-5 border-b border-[#FFD343]"
+            onPress={() => router.push(`/chat/${item.id}`)}>
+            <Text className="text-[#FFD343] text-lg font-bold">
+              {item.name}
+            </Text>
+            <Text className="text-gray-500">{item.lastMessage}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
 
-export default messages;
+export default ChatList;

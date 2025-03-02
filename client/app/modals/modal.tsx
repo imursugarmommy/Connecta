99cb0c6be545c9e5de364e764994ca19446e1c98
imursugarmomy <levi.besch@gmail.com>
2025-03-02 21:08:19 +0100

    return if library permission isnt granted

--- client/app/modals/modal.tsx
+++ client/app/modals/modal.tsx
@@ -71,9 +71,9 @@ const Modal = () => {
 
   const pickImage = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== "granted") {
-      alert("Sorry, we need camera roll permissions to make this work!");
-    }
+
+    if (status !== "granted")
+      return alert("Sorry, we need camera roll permissions to make this work!");
 
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images", "videos"],
